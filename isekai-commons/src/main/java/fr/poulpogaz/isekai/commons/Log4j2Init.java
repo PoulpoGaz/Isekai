b0e3a6116dc3747cc918633e52488b998477facb
PoulpoGaz <valent.carrez@gmail.com>
2022-02-09 20:40:28 +0100

    Fix creation of empty 'isekai.log' file

--- isekai-commons/src/main/java/fr/poulpogaz/isekai/commons/Log4j2Init.java
+++ isekai-commons/src/main/java/fr/poulpogaz/isekai/commons/Log4j2Init.java
@@ -1,7 +1,11 @@
 package fr.poulpogaz.isekai.commons;
 
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.config.Configurator;
+import org.apache.logging.log4j.core.config.builder.api.*;
+import org.apache.logging.log4j.core.config.builder.impl.BuiltConfiguration;
 
 import java.nio.file.Path;
 
@@ -9,9 +13,30 @@ public class Log4j2Init {
 
     public static void init(String module) {
         Path log = Cache.of("isekai-%s.log".formatted(module));
-        System.setProperty("log-path", log.toString());
 
-        ((LoggerContext) LogManager.getContext(false)).reconfigure();
+        ConfigurationBuilder<BuiltConfiguration> builder = ConfigurationBuilderFactory.newConfigurationBuilder();
+        builder.setStatusLevel(Level.INFO);
+
+        LayoutComponentBuilder layout = builder.newLayout("PatternLayout");
+        layout.addAttribute("pattern", "%d{yyyy-MM-dd HH:mm:ss.SSS} [%t] %-5level %logger{36} - %msg%n");
+
+        AppenderComponentBuilder console = builder.newAppender("stdout", "Console");
+        console.add(layout);
+
+        AppenderComponentBuilder file = builder.newAppender("file", "File");
+        file.addAttribute("fileName", log.toString());
+        file.addAttribute("append", true);
+        file.add(layout);
+
+        builder.add(console);
+        builder.add(file);
+
+        RootLoggerComponentBuilder rootLogger = builder.newRootLogger(Level.TRACE);
+        rootLogger.add(builder.newAppenderRef("stdout"));
+        rootLogger.add(builder.newAppenderRef("file"));
+        builder.add(rootLogger);
+
+        ((LoggerContext) LogManager.getContext(false)).reconfigure(builder.build());
         LogManager.getLogger("ROOT").info("=== STARTING {} ===", module);
     }
 
